3527a163db70541d0790c6d46836d2619759f9c3
Igor Brizack <igor_brizack@hotmail.com>
2022-03-07 19:29:53 -0300

    conversor de notas

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -102,18 +102,20 @@ switch (peca) {
 
 // Conversor de notas
 
-let nota = 90;
+let nota = 0;
 
-if (nota >= 90) {
+if (nota >= 90 && nota <= 100) {
     console.log("Nota A");
-} else if (nota >= 80){
+} else if (nota >= 80 && nota <= 89){
     console.log("Nota B");
-} else if (nota >= 70){
+} else if (nota >= 70 && nota <= 79){
     console.log("Nota C");
-} else if (nota >= 60){
+} else if (nota >= 60 && nota <= 69){
     console.log("Nota D");
+} else if (nota > 100 || nota < 0){
+    console.log("Nota inválida");
 } else {
-    console.log("F");
+    console.log("Nota F")
 }
 
 
