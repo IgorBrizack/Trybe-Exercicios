fd8b165d236dd119b9eccf0e15a6d1a38e4c06d2
Igor Brizack <igor_brizack@hotmail.com>
2022-03-09 19:36:09 -0300

    Dia 3 javaScript

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -0,0 +1,10 @@
+//Fatorial de 10
+ let numero = 10;
+ let fatorial = numero;
+ 
+
+ for(let index = 1; index < (numero - 1); index += 1){
+    fatorial = fatorial * (numero - index);
+    console.log(fatorial)
+} 
+ 
