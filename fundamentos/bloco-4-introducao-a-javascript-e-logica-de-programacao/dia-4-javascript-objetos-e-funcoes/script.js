885e68ed6ce493b79740a1b8bc90c9f75a31fb2d
Igor Brizack <igor_brizack@hotmail.com>
2022-03-10 20:56:29 -0300

    parte 6 do exercicio

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -1,34 +1,55 @@
-// Imprimir nome na tela
+// // Imprimir nome na tela
 
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+
+// console.log('Bem-vinda ' + info.personagem);
+
+// //Adicionar nova chace com dado = 'sim'
+// info['recorrente'] = 'sim';
+
+// console.log(info);
+
+// //Fazer um for in que mostre todas as chaves do objeto info
+
+// for (let key in info){
+//     console.log(key);
+// }
+
+// //fazer um novo objeto e imprimir na tela relacionado com o primeiro objeto da atv
+
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: 'O último MacPatinhas',
+// }
+
+// info2['recorrente'] = 'sim';
+
+// console.log(info['personagem'] + ' e ' + info2['personagem']);
+// console.log(info['origem'] + ' e ' + info2['origem']);
+// console.log(info['nota'] + ' e ' + info2['nota']);
+// console.log(info['recorrente']);
+
+
+// Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
 };
 
-console.log('Bem-vinda ' + info.personagem);
+leitor['fullName'] = leitor.nome + ' ' + leitor.sobrenome;
 
-//Adicionar nova chace com dado = 'sim'
-info['recorrente'] = 'sim';
-
-console.log(info);
-
-//Fazer um for in que mostre todas as chaves do objeto info
-
-for (let key in info){
-    console.log(key);
-}
-
-//fazer um novo objeto e imprimir na tela relacionado com o primeiro objeto da atv
-
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-}
-
-info2['recorrente'] = 'sim';
-
-console.log(info['personagem']);
-
-
+console.log('O livro favorito ' + leitor.fullName + ' se chama o ' + leitor.livrosFavoritos[0].titulo);
