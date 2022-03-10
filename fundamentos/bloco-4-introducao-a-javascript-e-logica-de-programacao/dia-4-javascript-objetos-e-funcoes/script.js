9189672f7c0e63ce776aafdb73410b4efdd1547b
Igor Brizack <igor_brizack@hotmail.com>
2022-03-10 18:55:27 -0300

    recorrente

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -0,0 +1,14 @@
+// Imprimir nome na tela
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+console.log('Bem-vinda ' + info.personagem);
+
+info['recorrente'] = 'sim';
+
+console.log(info);
+
