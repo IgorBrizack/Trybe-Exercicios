01a61782c6ac115a9ca784da5e75ece827993422
Igor Brizack <igor_brizack@hotmail.com>
2022-03-11 10:00:59 -0300

    maior string do meu array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
@@ -39,27 +39,69 @@
 // console.log(indice(maxValue, array));
 
 //Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
-let array = [2, 3, 6, 7, 10, 1];
+// let array = [2, 3, 6, 7, 10, 1];
 
-function getMinOfArray(numArray) {  
-    return Math.min.apply(null, numArray);
- }
+// function getMinOfArray(numArray) {  
+//     return Math.min.apply(null, numArray);
+//  }
 
- let minValue = getMinOfArray(array);
+//  let minValue = getMinOfArray(array);
 
 
- function indice(number1, myArray){
-    let answer;
-    let meuArray = myArray;
-    for(index = 0; index < meuArray.length; index += 1){
-        if(number1 === meuArray[index]){
-            answer = index;
+//  function indice(number1, myArray){
+//     let answer;
+//     let meuArray = myArray;
+//     for(index = 0; index < meuArray.length; index += 1){
+//         if(number1 === meuArray[index]){
+//             answer = index;
+//         }
+//     }
+//     return answer;
+// };
+
+// console.log(indice(minValue, array));
+
+//Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+let array =  ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+//carregar o tamanho das minhas string em uma nova aray;
+function stringSize(arrayValue){
+    let array = arrayValue;
+    let size = [];
+    for (let index = 0; index < array.length; index += 1){
+        let aux = array[index];
+        let cont = 0;
+        for(let index2 = 0; index2 < aux.length;index2 += 1){
+            cont += 1;
+        }
+        size.push(cont);
+    }
+    return size
+}
+
+let arraySize = (stringSize(array));
+
+//Agora eu vou pegar o maior valor do meu novo array utilizando a função da questão anteriror.
+function getMaxOfArray(numArray) {
+    return Math.max.apply(null, numArray);
+}
+
+//Agora que eu sei o maior valor do meu Array eu vou passar ele para uma nova variavel, para fazer a comparação e ver qual a minha maior string.
+let position = getMaxOfArray(arraySize)
+
+//Agora função para comparar os valores e ver em qual posição o valor Max bate com o da minha arraySize e retornar o nome.
+function wordSize(arraySize,position){
+    for(let index = 0; index < arraySize.length;index += 1){
+        if (position === arraySize[index]){
+            return array[index];
         }
     }
-    return answer;
-};
+}
 
-console.log(indice(minValue, array))
-
+console.log(wordSize(arraySize,position));
 
 
+
+
+    
