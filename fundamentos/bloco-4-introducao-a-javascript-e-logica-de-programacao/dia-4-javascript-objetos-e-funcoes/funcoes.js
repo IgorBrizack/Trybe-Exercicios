96b70e34759a9964b362e87ec12cae947c7981c6
Igor Brizack <igor_brizack@hotmail.com>
2022-03-11 09:18:03 -0300

    indice de menor valor

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
@@ -15,17 +15,40 @@
 
 //Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 
+// let array = [2, 3, 6, 7, 10, 1];
+
+// //função retirada https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+// //utilizada para identificar o maior valor do meu array
+// function getMaxOfArray(numArray) {
+//     return Math.max.apply(null, numArray);
+// }
+
+// let maxValue = getMaxOfArray(array);
+
+// function indice(number1, myArray){
+//     let answer;
+//     let meuArray = myArray;
+//     for(index = 0; index < meuArray.length; index += 1){
+//         if(number1 === meuArray[index]){
+//             answer = index;
+//         }
+//     }
+//     return answer;
+// };
+
+// console.log(indice(maxValue, array));
+
+//Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 let array = [2, 3, 6, 7, 10, 1];
 
-//função retirada https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max
-//utilizada para identificar o maior valor do meu array
-function getMaxOfArray(numArray) {
-    return Math.max.apply(null, numArray);
-}
+function getMinOfArray(numArray) {  
+    return Math.min.apply(null, numArray);
+ }
 
-let maxValue = getMaxOfArray(array);
+ let minValue = getMinOfArray(array);
 
-function indice(number1, myArray){
+
+ function indice(number1, myArray){
     let answer;
     let meuArray = myArray;
     for(index = 0; index < meuArray.length; index += 1){
@@ -36,8 +59,7 @@ function indice(number1, myArray){
     return answer;
 };
 
+console.log(indice(minValue, array))
 
 
 
-
-console.log(indice(maxValue, array));
