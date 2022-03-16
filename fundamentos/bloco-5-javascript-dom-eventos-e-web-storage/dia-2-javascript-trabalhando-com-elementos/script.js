a427a18312ab063b52088fb73096ca8e93837a5e
Igor Brizack <igor_brizack@hotmail.com>
2022-03-16 09:55:13 -0300

    andamento atividade Bloco 5 dia 2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -38,7 +38,62 @@ function createP(){
 createP();
 
 // Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+function sectionLeftContent(){
+    let main = document.querySelector('main');
+    let section = document.createElement('section');
+    section.className = 'left-content';
+    main.appendChild(section);
+}
+sectionLeftContent()
 // Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+function sectionRightContent(){
+    let main = document.querySelector('main');
+    let section = document.createElement('section');
+    section.className = 'rigth-content';
+    main.appendChild(section);
+}
+sectionRightContent()
+
+
 // Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do section criado no passo 5;
+function newImage(){
+    let leftContent = document.querySelector('.left-content');
+    let image = document.createElement('img')
+    image.className = 'small-image';
+    image.src = 'https://picsum.photos/200'
+    leftContent.appendChild(image);
+}
+newImage()
+
 // Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um , dois , três , ... como valores da lista. Essa lista deve ser filha do section criado no passo 6;
+function newList(){
+    let rigthContent = document.querySelector('.rigth-content');
+    let ulList = document.createElement('ul')
+    ulList.id = 'mainList';
+    rigthContent.appendChild(ulList);
+
+    for(let index = 1; index <= 10; index += 1){
+        let ulListMain = document.querySelector('#mainList');
+        let lists = document.createElement('li');
+        ulListMain.appendChild(lists);
+    }
+}
+newList()
+
 // Adicione 3 tags h3 , todas sendo filhas do main criado no passo 2.
+
+function addTagH3(){
+    for (let index = 0; index < 3; index += 1){
+        let main = document.querySelector('main');
+        let tagH3 = document.createElement('h3');
+        main.appendChild(tagH3);
+    }
+}
+addTagH3()
+
+// Adicione a classe title na tag h1 criada;
+// Adicione a classe description nas 3 tags h3 criadas;
+// Remova a section criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
+// Centralize a section criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto da section ;
+// Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
+// Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
