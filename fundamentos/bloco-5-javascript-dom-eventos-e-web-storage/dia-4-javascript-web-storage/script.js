2da1a88b5f1f4ef8fd7c0100271ad81a1595aaac
Igor Brizack <igor_brizack@hotmail.com>
2022-03-18 10:21:07 -0300

    concluindo atv

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -2,13 +2,11 @@
 
 let usuarios = [{
     nome: 'Igor',
-    tema: 'darkMode',
     fontSize: '15px',
     colorText: 'white',
     fontFamily: 'Arial'
 }, {
     nome: 'Julia',
-    tema: 'lightMode',
     fontSize: '25px',
     colorText: 'black',
     fontFamily: 'tahoma'
@@ -20,38 +18,45 @@ function createButton(){
     let inputSection = document.getElementById('inputs');
     let button = document.createElement('button');
     button.innerText = 'Usuario';
-    button.className = 'button';
+    button.id = 'button';
     inputSection.appendChild(button);
 }
 createButton();
 
 
-let buttonUsuario = document.getElementsByTagName('button');
-buttonUsuario.addEventListener('clic', getUser);
+let buttonUsuario = document.getElementById('button');
+buttonUsuario.addEventListener('click', getUser);
 
 function getUser(){
     for(let index = 0; index < usuarios.length; index += 1){
         if(document.getElementById('input-text').value === usuarios[index].nome){
             let nome = usuarios[index].nome;
-            let tema = usuarios[index].tema;
             let fontSize = usuarios[index].fontSize;
             let colorText = usuarios[index].colorText;
             let fontFamily = usuarios[index].fontFamily;
-            checkPreference(nome, tema, fontSize, colorText, fontFamily)
-        } else {
-            window.alert('Usuario não encontrado!');
-        }
+            checkPreference(nome, fontSize, colorText, fontFamily)
+        } 
     }    
 }
 
-function checkPreference(nome, tema, fontSize, colorText, fontFamily){
+function checkPreference(nome, fontSize, colorText, fontFamily){
     localStorage.setItem('nome', nome);
-    localStorage.setItem('tema', tema);
     localStorage.setItem('fontSize', fontSize);
     localStorage.setItem('colorText', colorText);
     localStorage.setItem('fontFamily', fontFamily);
+    // execTheme();
 }
 
 
+// function execTheme(){
+//     let fontSize = localStorage.getItem('fontSize');
+//     fontSize(fontSize);
+// }
 
+// function fontSize(font){
+//     let texts = document.getElementsByClassName('texts');
+//     console.log(texts)
+//     for(let index = 0; index < texts.length; index += 1){
 
+//     }
+// }
