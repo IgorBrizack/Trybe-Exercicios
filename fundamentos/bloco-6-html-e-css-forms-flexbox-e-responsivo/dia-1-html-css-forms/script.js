68999c5e006a8ed3f1341d29bc271c368cf49c52
Igor Brizack <igor_brizack@hotmail.com>
2022-03-25 11:39:36 -0300

    atividade concluida

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,7 @@
+// const submitButton = document.getElementById("send-text");
+// submitButton.addEventListener('click', submitForms)
+
+// function submitForms(e){
+//     e.preventDefault();
+    
+// }
