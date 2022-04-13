dceb1a92783db24f95a8bbb5eaa9524c67d7f911
Igor Brizack <igor_brizack@hotmail.com>
2022-04-13 11:16:06 -0300

    conclusão

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise6.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise6.js
@@ -1,7 +1,7 @@
 const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
-const studentAverage = students.map((student, index, array) => {
+const studentAverage = students.map((student, index) => {
 	return {
 		name: student,
 		average: (grades[index].reduce((acc, grade) => {
